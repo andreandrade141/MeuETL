18b9c3078f94a728682ddf30f463f801edca8bd0
andreandrade141 <andrelsandrade43@gmail.com>
2022-07-04 22:35:17 -0300

    Adiciona input por args

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
+import sys
 from operation import ApiScrape
 
 
-def main() -> None:
+def main(qtd: int, fname: str, book: str, author: str) -> None:
     client = ApiScrape(url='https://www.abibliadigital.com.br/api/books')
 
-    client.execute(quantity=50, f_name='extract')
+    client.execute(qtd, fname)
 
-    client.query_by_book('Rute')
-    client.query_by_author('Samuel')
+    client.query_by_book(book)
+    client.query_by_author(author)
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]), sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print('Usage: run.py <qtd> <fname> <book> <author>')
